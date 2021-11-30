8bdc48636e405fafbb631d58d2378e2b1147cde5
JohnCarreos <jrcarreos1@gmail.com>
2021-11-30 16:11:29 +0800

    added test for model attributes

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should include the "category_id" attribute' do
+    expect(subject.attributes).to include("category_id") 
+  end
+
+  it 'should include the "name" attribute' do
+    expect(subject.attributes).to include("name")
+  end
+
+  it 'should include the "body" attribute' do
+    expect(subject.attributes).to include("body")
+  end
+
+  it 'should include the "task_date" attribute' do
+    expect(subject.attributes).to include("task_date")
+  end
 end
