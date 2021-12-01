d5a37ae52d6218a926f7f4a957f48cd0dd5dea15
JohnCarreos <jrcarreos1@gmail.com>
2021-12-01 16:44:17 +0800

    added test for task model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -16,4 +16,27 @@ RSpec.describe Task, type: :model do
   it 'should include the "task_date" attribute' do
     expect(subject.attributes).to include("task_date")
   end
+
+  it 'not valid without name' do
+    task.category_id = 1
+    task.name = nil
+    task.body = 'Body of Task'
+
+    expect(task).to_not be_valid
+    expect(task.errors).to be_present
+    expect(task.errors.to_h.keys).to include(:name)
+  end
+
+  it 'not valid without body' do
+    task.category_id = 1
+    task.name = 'Body of Task'
+    task.body = nil
+
+    expect(task).to_not be_valid
+    expect(task.errors).to be_present
+    expect(task.errors.to_h.keys).to include(:body)
+  end
+
 end
+
+  
